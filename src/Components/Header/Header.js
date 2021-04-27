7e62726fc94e375567c77d31753f0d4598372d42
Varun Khalate <khalatevarun@gmail.com>
2021-04-27 20:27:38 +0530

    Add ThemeProvider dark theme

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import './Header.css';
-import { TextField } from '@material-ui/core';
+import { createMuiTheme, TextField, ThemeProvider } from '@material-ui/core';
 
 const Header = () => {
+  const darkTheme = createMuiTheme({
+    palette: {
+      primary: {
+        main: '#ffff',
+      },
+      type: 'dark',
+    },
+  });
   return (
     <div className="header">
       <span className="title">Word Hunt</span>
       <div className="inputs">
-        <TextField id="standard-basic" label="Standard" />
+        <ThemeProvider theme={darkTheme}>
+          <TextField id="standard-basic" label="Standard" />
+        </ThemeProvider>
       </div>
     </div>
   );
