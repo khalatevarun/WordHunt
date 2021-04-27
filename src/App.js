1eacdbdee377f51a9c8c607299d6bd83d25e92c5
Varun Khalate <khalatevarun@gmail.com>
2021-04-27 19:28:54 +0530

    Check whether api is live

--- src/App.js
+++ src/App.js
@@ -1,4 +1,22 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+
 function App() {
+  const dictionaryApi = async () => {
+    try {
+      const data = await axios.get(
+        'https://api.dictionaryapi.dev/api/v2/entries/en/plane',
+      );
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    dictionaryApi();
+  }, []);
+
   return <div>Hello World!</div>;
 }
 
